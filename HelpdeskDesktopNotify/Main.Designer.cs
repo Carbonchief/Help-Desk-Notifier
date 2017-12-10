e3fc7b7438ad5c50d40642f3b0faa36dbb0ad917
Luan van der Walt <luan.v@Raubex.com>
2017-12-10 18:33:42 +0200

    Added Icon

---
+++ HelpdeskDesktopNotify/Main.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace HelpdeskDesktopNotify
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.nIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.btnOpenHelpdesk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nIcon
+            // 
+            this.nIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.nIcon.BalloonTipText = "Notifier has been minimized to System Tray";
+            this.nIcon.BalloonTipTitle = "Helpdesk Notifier";
+            this.nIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("nIcon.Icon")));
+            this.nIcon.Text = "Raubex Helpdesk";
+            this.nIcon.BalloonTipClicked += new System.EventHandler(this.nIcon_BalloonTipClicked);
+            this.nIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.nIcon_MouseDoubleClick);
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Location = new System.Drawing.Point(12, 9);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(35, 13);
+            this.lblMaxValue.TabIndex = 0;
+            this.lblMaxValue.Text = "label1";
+            // 
+            // btnOpenHelpdesk
+            // 
+            this.btnOpenHelpdesk.Location = new System.Drawing.Point(15, 25);
+            this.btnOpenHelpdesk.Name = "btnOpenHelpdesk";
+            this.btnOpenHelpdesk.Size = new System.Drawing.Size(100, 23);
+            this.btnOpenHelpdesk.TabIndex = 1;
+            this.btnOpenHelpdesk.Text = "Open Helpdesk";
+            this.btnOpenHelpdesk.UseVisualStyleBackColor = true;
+            this.btnOpenHelpdesk.Click += new System.EventHandler(this.btnOpenHelpdesk_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnOpenHelpdesk);
+            this.Controls.Add(this.lblMaxValue);
+            this.Name = "Main";
+            this.Text = "Helpdesk Notifier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon nIcon;
+        private System.Windows.Forms.Label lblMaxValue;
+        private System.Windows.Forms.Button btnOpenHelpdesk;
+    }
+}
+
---
+++ HelpdeskDesktopNotify/Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace HelpdeskDesktopNotify
+{
+    public partial class Main : Form
+    {
+        string userName = "";
+        int MaxValue = 0;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+                nIcon.Visible = true;
+                nIcon.BalloonTipText = "Notifier has been minimized to System Tray";
+                nIcon.ShowBalloonTip(1000);
+            }
+        }
+
+        private void nIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowInTaskbar = true;
+            nIcon.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            MaxValue = GetData();
+            lblMaxValue.Text = "Your latest Helpdesk Call is: " + MaxValue.ToString();
+            var timer = new System.Timers.Timer(60000);
+
+            // Hook up the Elapsed event for the timer.
+            timer.Elapsed += Timer_Elapsed;
+
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {           
+            var value = GetData();
+            if (MaxValue < value)
+            {
+                nIcon.BalloonTipText = "Call " + value + " has been assign to you. " + userName;
+                nIcon.ShowBalloonTip(1000);
+                MaxValue = value;
+                lblMaxValue.BeginInvoke((MethodInvoker)delegate () { lblMaxValue.Text = "Your latest Helpdesk Call is: " + MaxValue.ToString(); ; });
+            }
+        }
+
+        private int GetData()
+        {
+
+            using (SqlConnection connection = new SqlConnection("Server=10.1.1.7;Database=RAUBEX_HELPDESK;Trusted_Connection=True;"))
+            {
+                connection.Open();
+                string query = "SELECT Max([id]) as Latest FROM [RAUBEX_HELPDESK].[dbo].[REQUESTS_TBL] where responsibility = '" + userName.Remove(0, 4) + "'";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    return (int)command.ExecuteScalar();
+
+                }
+            }
+        }
+
+        private void nIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://helpdesk.raubex.com/Submitrequest.aspx?iREQNUMB=" + MaxValue);
+            Process.Start(sInfo);
+        }
+
+        private void btnOpenHelpdesk_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://helpdesk.raubex.com");
+            Process.Start(sInfo);
+        }
+    }
+
+
+}
